2eda6b66fb7febf35b16410663c0cb08275ade44
Elia <quelviscidosnake@pop-os.homenet.telecomitalia.it>
2026-06-24 23:06:00 +0200

    migliorato il file .c, ora i file buffer vengono creati subito, cioè appena il servizio viene avviato. mihliorata anche la gestione degli errori grazie alla riscrittura della funzione die, ora le stringhe di errore finiscono direttamente nella stderr, tali errori saranno poi leggibili sui log di sistema consultabili con journalctl ma anche sullo status del servizio

--- clipboard-daemon/cpd.c
+++ clipboard-daemon/cpd.c
@@ -5,113 +5,129 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <errno.h>
 
-void die(const char *s) {
-  perror(s);
-  exit(1);
+FILE *fp = NULL;
+FILE *fb = NULL;
+
+void die(const char *s, const char *serrno) {
+	// scrivi l'errore sulla stderr
+	if(serrno==NULL) fprintf(stderr, "<3> ERROR: %s\n", s);
+	else fprintf(stderr, "<3> ERROR: %s\n, errno error string: %s\n", s, serrno);
+
+	// forza la chiusura dei file buffer aperti, devi verificare se prima sono aperti
+	if(fp != NULL) pclose(fp);
+	if(fb != NULL) fclose(fb);
+	
+	exit(1);
 }
 
 int main() {
-    
-    Display *disp;
-    Window root;
-    Atom clip;
-    XEvent evt;
-    int opt;
+	
+	Display *disp;
+	Window root;
+	Atom clip;
+	XEvent evt;
+	int opt;
 
-    static int loop = 1;
-    int selections = (1 << 0); //clipboard
-    char* logfile = "./cpd-log.log";
-    char* bufferfileA = "/dev/shm/cpdA.b";
-    char* bufferfileB = "/dev/shm/cpdB.b";
-    int count = 1;
+	static int loop = 1;
+	int selections = (1 << 0); //clipboard
+	char* bufferfileA = "/dev/shm/cpdA.b";
+	char* bufferfileB = "/dev/shm/cpdB.b";
+	int count = 1;
 
-    FILE *fp;
-    FILE *fb;
-    
-    disp = XOpenDisplay(NULL);
-    if (!disp) die("can't open X display\n");
+	
+	disp = XOpenDisplay(NULL);
+	if (!disp) die("can't open X display\n",NULL);
 
-    root = DefaultRootWindow(disp);
+	root = DefaultRootWindow(disp);
 
-    clip = XInternAtom(disp, "CLIPBOARD", False);
+	clip = XInternAtom(disp, "CLIPBOARD", False);
 
-    XFixesSelectSelectionInput(disp, root, clip, XFixesSetSelectionOwnerNotifyMask);
-    
-    (void)setvbuf(stdout, NULL, _IONBF, 0);
+	XFixesSelectSelectionInput(disp, root, clip, XFixesSetSelectionOwnerNotifyMask);
+	
+	(void)setvbuf(stdout, NULL, _IONBF, 0);
+	
+	// creo subito i due file buffer, non è fondamentale per il funzionamento dell'algoritmo ma serve maggiormente
+	// per interfaccia bash, potrei effettivamente gestire questa cosa da quella parta ma servirebbero controlli e inoltre
+	// verrebbero eseguiti comandi, quindi forse è più ottimizzato qui
+	fb = fopen(bufferfileA, "w");
+	if (fb == NULL) die("error to open file buffer A", strerror(errno));
+	if (fclose(fb)) die("error to close file buffer A", strerror(errno));
+	fb = fopen(bufferfileB, "w");
+	if (fb == NULL) die("error to open file buffer B", strerror(errno));
+	if (fclose(fb)) die("error to close file buffer B", strerror(errno));
 
-    do {
-        
-        XNextEvent(disp, &evt); // suppongo che il codice si blocchi qui
-        
-        FILE *fp;
-        char buffer[1024];
-        int i, ch;
+	fb = NULL;
+	
+	do {
+		
+		XNextEvent(disp, &evt); // suppongo che il codice si blocchi qui
+		
+		char buffer[1024];
+		int i, ch;
 
-        // Eseguiamo xclip in modalità lettura (-o)
-        // "r" significa che vogliamo LEGGERE l'output del comando
-        fp = popen("xclip -o -selection clipboard 2>/dev/null", "r");
-        if (fp == NULL) die("error in popen");
-        
-        for (i = 0; (i < (sizeof(buffer)-1) && ((ch = fgetc(fp)) != EOF)); i++)
-            buffer[i] = ch;
-     
-        buffer[i] = '\0';
-        if (fclose(fp)) die("errore to close command buffer");
-          
-        FILE *fb;
-        if (count == 10){
-            fb = fopen(bufferfileB, "w");
-            if (fb == NULL) die("error to open file buffer");
-            if (fclose(fb)) perror("error to close file buffer");
-            fb = fopen(bufferfileA, "ab+");
-            if (fb == NULL) die("error to open file buffer");
-            if(!fprintf(fb, "%s%c", buffer, '\0')) die("error to write file buffer");
-            count++;
-        } else if (count == 20){
-            fb = fopen(bufferfileA, "w");
-            if (fb == NULL) die("error to open file buffer");
-            if (fclose(fb)) perror("error to close file buffer");
-            fb = fopen(bufferfileB, "ab+");
-            if (fb == NULL) die("error to open file buffer");
-            if(!fprintf(fb, "%s%c", buffer, '\0')) die("error to write file buffer");
-            count = 1;
-        } else if (count < 10){
-            fb = fopen(bufferfileA, "ab+");
-            if (fb == NULL) die("error to open file buffer");
-            if(!fprintf(fb, "%s%c", buffer, '\0')) die("error to write file buffer");
-            count++;
-        } else if (count > 10){
-            fb = fopen(bufferfileB, "ab+");
-            if (fb == NULL) die("error to open file buffer");
-            if(!fprintf(fb, "%s%c", buffer, '\0')) die("error to write file buffer");
-            count++;
-        }
-        if (fclose(fb)) perror("error to close file buffer");
-        
+		// Eseguiamo xclip in modalità lettura (-o)
+		// "r" significa che vogliamo LEGGERE l'output del comando
+		fp = popen("xclip -o -selection clipboard 2>/dev/null", "r");
+		if (fp == NULL) die("error in popen", strerror(errno));
+		
+		for (i = 0; (i < (sizeof(buffer)-1) && ((ch = fgetc(fp)) != EOF)); i++)
+			buffer[i] = ch;
+	 
+		buffer[i] = '\0';
+		if (pclose(fp) == -1) die("errore to close command buffer", strerror(errno));
+		  
+		if (count == 10){
+			fb = fopen(bufferfileB, "w");
+			if (fb == NULL) die("error to open file buffer B", strerror(errno));
+			if (fclose(fb)) die("error to close file buffer B", strerror(errno));
+			fb = fopen(bufferfileA, "ab+");
+			if (fb == NULL) die("error to open file buffer A", strerror(errno));
+			if(!fprintf(fb, "%s%c", buffer, '\0')) die("error to write file buffer A", strerror(errno));
+			count++;
+		} else if (count == 20){
+			fb = fopen(bufferfileA, "w");
+			if (fb == NULL) die("error to open file buffer A", strerror(errno));
+			if (fclose(fb)) die("error to close file buffer A", strerror(errno));
+			fb = fopen(bufferfileB, "ab+");
+			if (fb == NULL) die("error to open file buffer B", strerror(errno));
+			if(!fprintf(fb, "%s%c", buffer, '\0')) die("error to write file buffer B", strerror(errno));
+			count = 1;
+		} else if (count < 10){
+			fb = fopen(bufferfileA, "ab+");
+			if (fb == NULL) die("error to open file buffer A", strerror(errno));
+			if(!fprintf(fb, "%s%c", buffer, '\0')) die("error to write file buffer A", strerror(errno));
+			count++;
+		} else if (count > 10){
+			fb = fopen(bufferfileB, "ab+");
+			if (fb == NULL) die("error to open file buffer B", strerror(errno));
+			if(!fprintf(fb, "%s%c", buffer, '\0')) die("error to write file buffer B", strerror(errno));
+			count++;
+		}
+		if (fclose(fb)) die("error to close file buffer", strerror(errno));
+		
 
-    } while (1);
-    
-    XCloseDisplay(disp);
-    
-    return 0;
+	} while (1);
+	
+	XCloseDisplay(disp);
+	
+	return 0;
 }
 
 
 
 
+// comando per reload dei servizi di systemctl
+// sudo systemctl daemon-reload
+ 
+ // comandi pef monitorare e gestire il servizio
+ // systemctl --user start cpd
+ // systemctl --user stop cpd
+ // systemctl --user status cpd
 
-
-
-
-// comando per estrarre dal file buffer le stringhe
-// awk -v RS='\0' 'NR==2 {print; exit}' cpd.d
-//  
-// 
-// 
-// comando per avviare lo script e inviare gli errori nel file di log
-// ./cpd 2>> cpd-error.log
-
+// comando che dovrebbe mostrare messaggi di errore scritti dal servizio
+// journalctl --user -u cpd -p err
 
 
 
