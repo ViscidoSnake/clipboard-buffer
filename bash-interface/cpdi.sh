c857aaffff80e5d4c450a34631947c7cbcb29df5
Elia <quelviscidosnake@pop-os.homenet.telecomitalia.it>
2026-06-25 10:13:39 +0200

    Modifiche nel file bash, inserito controllo che scrive una stringa di avviso quando si cercano di ottenere le 10 stringhe nel buffer ma in realtà queste non sono ancora presenti.
    Riscritti meglio alcuni commenti
    
    Inizio definizione e gestione del flag -c che permette di ottenere lo stato del demone e anche start stop
    (forse anche altro, ci sto ancora pensando)

--- bash-interface/cpdi.sh
+++ bash-interface/cpdi.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
-
+# variabili globali, files sarebbe un array che contiene il percorso dei due file usati come buffer, findex viene usata sempre come indice
+# dell'array files, rindex sarebbe l'indice che identifica una certa stringa in uno dei file buffer. tmpfile contiene il percorso di un file
+# temporaneo che serve per alcune operazioni rapide come la rimozione di stringhe dal buffer
 files=("/dev/shm/cpdA.b" "/dev/shm/cpdB.b")
 findex=0
 rindex=1
+tmpfile="/dev/shm/tmp.b"
 
-#index
+
+# argomento: $rindex. La funzione converte le posizioni in indici che servono poi per ottenere l'effettiva stringa nei file
+# buffer, la funzione imposta i valori delle due variabili globali findex e rindex !!!
 transform_ring_position_in_index(){
 	local position=$1
 
@@ -60,10 +65,12 @@ transform_ring_position_in_index(){
 }
 
 
-######
+################### GESTIONE degli input (glag e argomenti) attraverso struttura a switch case ###################
 
-rposition=1 #valore di default
+
+rposition=1
 remove=false
+dcommand="default"
 
 while [[ $# -gt 0 ]]
 do
@@ -72,7 +79,7 @@ do
         	if [ $# -lt 2 ]
         	then
 				echo "-i request an argument between 0-10"
-        		exit 1
+        		exit 0
         	fi
 			if [ $2 -ge 0 ] && [ $2 -le 10 ] #ge, ricorda è maggiore uguale! 
             then
@@ -84,20 +91,76 @@ do
             remove=true
             shift
             ;;
+        -c)	
+        	if [ $# -lt 2 ]
+        	then
+				echo "-c request an argument: start|stop|status|restart"
+        		exit 0
+        	fi
+			if [ "$2" = "start" ] || [ "$2" = "stop" ] || [ "$2" = "status" ] || [ "$2" = "restart" ] 
+            then
+            	dcommand=$2
+			fi
+			# break
+			shift 2
+            ;;
         *)
             echo "Argomento sconosciuto: $1"
-            exit 1
+            exit 0
             ;;
     esac
 done
+# potrei volendo uscire subito una volta processato il flag -c perche gli altri flag poi non mi servirebbero per l'elaborazione essendo
+# che prevedo di fermare l'esecuzione prima che le variabili impostate da questi vengano processate però non faccio break perchè comunque
+# nel while gli argomenti vengono validati quindi se viene scritto qualcosa di senza senso dopo il flag -i comunque questo viene segnalato
+# e l'esecuzione termina, diciamo quindi che è più per una questione di coerenza nel senso che l'utente ha sempre lo stesso feedback da parte
+# del programma per quanto riguarda la sintassi dei comandi.... 
 
+
+##################################################################################################################
+
+
+
+
+#### GESTIONE flag -c quindi comandi per interagire con il demone, (in realtà con systemctl che poi interviene su esso) ####
+if [ "$dcommand" != "default" ]
+then
+	`systemctl --user $dcommand cpd`
+	
+	exit 0
+fi
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#### GESTIONE flag -i e -r quindi praticamente stampa e rimozione delle stringhe ####
 
 
 # if che gestisce i casi in cui -i ha argomento 0
 if [ $rposition -eq 0 ] && [ $remove == false ] # caso 0, stampa le ultime 10 stringhe in memoria però solo se in AND con remove negato
 then									
 	transform_ring_position_in_index 1
-	
+
+	# if che verifica se effettivamente ci siano elemnti da stampare, in caso contrario scrive una stringa che avverte del fatto che non 
+	# siano presenti stringhe
+	if [ $rindex -eq -1 ]
+	then
+		echo "actualy, the buffer is empty"
+		exit 0
+	fi 
+
 	out=`awk -v RS='\0' '
 	{
 	    rec[++n] = $0
@@ -115,6 +178,9 @@ then
 	exit 0
 elif [ $rposition -eq 0 ] && [ $remove == true ]
 then
+	# potrebbe aver senso fare il controllo di prima cioè se non ci sono stringhe da stampare meglio uscire subito e scrivere un avviso che
+	# informa del fatto che non sono state "rimosse" stringhe perche effettivamente non ce ne sono tuttavia il codice sotto non da problemi
+	# anche se eseguito in questo modo quindi per ora lascio così
 	`awk -v RS='\0' -v ORS='\0' '
 	 	{
 		    print "\x1B[1;31;49mString removed!\x1B[0m"
@@ -123,7 +189,7 @@ then
 		{
 		    print
 		}
-		' "${files[$findex]}" > /dev/shm/tmp.b | mv /dev/shm/tmp.b "${files[$findex]}"`
+		' "${files[$findex]}" > "$tmpfile" | mv "$tmpfile" "${files[$findex]}"`
 		`awk -v RS='\0' -v ORS='\0' -v idx="$rindex" '
 	 	{
 		    print "\x1B[1;31;49mString removed!\x1B[0m"
@@ -132,13 +198,16 @@ then
 		{
 		    print
 		}
-		' "${files[$((1-findex))]}" > /dev/shm/tmp.b | mv /dev/shm/tmp.b "${files[$((1-findex))]}"`
+		' "${files[$((1-findex))]}" > "$tmpfile" | mv "$tmpfile" "${files[$((1-findex))]}"`
 	exit 0
 fi
 
 
-# if che gestisce i casi in cui -i ha argomento comporeso tra 1 e 10
+# a questo punto del codice rposition è comporeso tra 1 e 10, ricavo rindex usando la funzione apposita
 transform_ring_position_in_index "$rposition"
+
+# il controllo che segue verifica il valore di rindex, se negativo significa che la posizione richiesta ancora non esiste nel buffer
+# cioè deve ancora essere scritta e pertanto mi fermo, non ha senso proseguire cercando di operare su una stringa che non esiste 
 if [ $rindex -lt 0 ]
 then
 	echo "actualy, the position $rposition isn't written"
@@ -159,7 +228,7 @@ else
 	{
 		print
 	}
-	' "${files[$findex]}" > /dev/shm/tmp.b | mv /dev/shm/tmp.b "${files[$findex]}"`
+	' "${files[$findex]}" > "$tmpfile" | mv "$tmpfile" "${files[$findex]}"`
 fi
  	
  
