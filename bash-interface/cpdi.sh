839f60e7d0f629ff58947120c1aa1b9044a89bb0
Elia <quelviscidosnake@pop-os.homenet.telecomitalia.it>
2026-07-05 16:52:39 +0200

    aggiunta funzione exit_log e anche variabile globale logmsg che raccoglie i vari messaggi testuali che possono verificarsi durante l'esecuzione del programma. Aggiunto anche il flag -v (variabile verbose), sono stati definiti diversi codici di exit in modo che sia possibile capire come è terminato il programma. La funzione exit_log viene eseguita sempre dopo il verificarsi dell'exit grazie alla trap.

--- bash-interface/cpdi.sh
+++ bash-interface/cpdi.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+trap 'exit_status=$?; exit_log $exit_status' EXIT
+
 # variabili globali, files sarebbe un array che contiene il percorso dei due file usati come buffer, findex viene usata sempre come indice
 # dell'array files, rindex sarebbe l'indice che identifica una certa stringa in uno dei file buffer. tmpfile contiene il percorso di un file
 # temporaneo che serve per alcune operazioni rapide come la rimozione di stringhe dal buffer
@@ -8,6 +10,12 @@ findex=0
 rindex=1
 tmpfile="/run/user/1000/clipboard-buffer/tmp.b"
 
+logmsg=""
+
+rposition=1
+remove=false
+load=false
+verbose=false
 
 # argomento: $rindex. La funzione converte le posizioni in indici che servono poi per ottenere l'effettiva stringa nei file
 # buffer, la funzione imposta i valori delle due variabili globali findex e rindex !!!
@@ -65,63 +73,37 @@ transform_ring_position_in_index(){
 }
 
 
-################### GESTIONE degli input (glag e argomenti) attraverso struttura a switch case ###################
+exit_log(){
+	local exitmsg=""
+	case "$1" in
+	0)
+		exitmsg="All good, no error occurred"
+		;;
+	2)
+		exitmsg="Unexpecting flag used"
+		;;
+	3)
+		exitmsg="Buffer files aren't found, watch if daemon is running!"
+		;;
+	4)
+		exitmsg="Actually, the buffer is empty"
+		;;
+	5)
+		exitmsg="Actually, the position specified isn't written"
+		;;
+	*)
+		exitmsg="Unexpected exit code, unknown status"
+		;;
+	esac
+	
+	logmsg="${logmsg}\n${exitmsg}"
+	
+	if [ $verbose = true ]
+	then	
+		echo -e "$logmsg"
+	fi
+}
 
-
-rposition=1
-remove=false
-load=false
-
-while [[ $# -gt 0 ]]
-do
-    case "$1" in
-        -i)	
-        	if [ $# -lt 2 ]
-        	then
-				echo "-i request an argument between 0-10, default used (1)"
-				shift 1
-        	elif [ $2 -gt 10 ] || [ $2 -lt 0 ]
-        	then
-				echo "-i request an argument between 0-10, default used (1)"
-				shift 2
-			else
-				rposition=$2
-				shift 2
-        	fi
-            ;;
-        -r)
-            remove=true
-            shift 1
-            ;;
-        -l)
-            load=true
-            shift 1
-            ;;
-        *)
-            echo "Argomento sconosciuto: $1"
-            exit 0
-            ;;
-    esac
-done
-# potrei volendo uscire subito una volta processato il flag -c perche gli altri flag poi non mi servirebbero per l'elaborazione essendo
-# che prevedo di fermare l'esecuzione prima che le variabili impostate da questi vengano processate però non faccio break perchè comunque
-# nel while gli argomenti vengono validati quindi se viene scritto qualcosa di senza senso dopo il flag -i comunque questo viene segnalato
-# e l'esecuzione termina, diciamo quindi che è più per una questione di coerenza nel senso che l'utente ha sempre lo stesso feedback da parte
-# del programma per quanto riguarda la sintassi dei comandi.... 
-
-
-##################################################################################################################
-
-
-# da questo punto in poi si usano comandi che vogliono accedere al contenuto dei file buffer per questo motivo serve allora un
-# un controllo sul fatto che questi esistano oppure no, farlo ora evita di dover gestire poi errori strani che possono verificarsi
-# successivamente e rimanere occulti. NOTA: ricorda che il fatto che i file non esistano è un forte segnale che il demone non sta
-# venendo eseguito perchè il .service, al momento dell'avvio crea sempre questi file.
-# Verifica che i file esistano (rimuovi il commento se necessario)
-if [ ! -f "${files[0]}" ] || [ ! -f "${files[1]}" ]; then
-    echo "Buffer files aren't found, watch if daemon is running!"
-    exit 2
-fi
 
 # Funzione ausiliaria per gestire la stampa/caricamento dell'output
 # Evita la duplicazione tra echo e xclip
@@ -141,14 +123,62 @@ dispatch_output() {
 		}
 		{ print }
 		' "${files[$findex]}" > "$tmpfile" && mv "$tmpfile" "${files[$findex]}"
+
+		logmsg="${logmsg}\nSpecified strings have been loaded in clipboard"
     else
-		if [ $remove = false ]
-        then
-        	echo "$content"
-        fi
+    	echo "$content"
     fi
 }
 
+
+################### GESTIONE degli input (glag e argomenti) attraverso struttura a switch case ###################
+
+while [[ $# -gt 0 ]]
+do
+    case "$1" in
+        -i)	
+        	if [ $# -lt 2 ]
+        	then
+        		logmsg="${logmsg}\n-i request an argument between 0-10, default used (1)"
+				shift 1
+        	elif [ $2 -gt 10 ] || [ $2 -lt 0 ]
+        	then
+        		logmsg="${logmsg}\n-i request an argument between 0-10, default used (1)"
+				shift 2
+			else
+				rposition=$2
+				shift 2
+        	fi
+            ;;
+        -r)
+            remove=true
+            shift 1
+            ;;
+        -l)
+            load=true
+            shift 1
+            ;;
+        -v)
+        	verbose=true
+            shift 1
+            ;;
+        *)
+            exit 2
+            ;;
+    esac
+done
+
+
+##################################################################################################################
+
+# da questo punto in poi si usano comandi che vogliono accedere al contenuto dei file buffer per questo motivo serve allora un
+# un controllo sul fatto che questi esistano oppure no, farlo ora evita di dover gestire poi errori strani che possono verificarsi
+# successivamente e rimanere occulti. NOTA: ricorda che il fatto che i file non esistano è un forte segnale che il demone non sta
+# venendo eseguito perchè il .service, al momento dell'avvio crea sempre questi file.
+# Verifica che i file esistano (rimuovi il commento se necessario)
+if [ ! -f "${files[0]}" ] || [ ! -f "${files[1]}" ]; then
+    exit 3
+fi
 
 
 
@@ -157,8 +187,7 @@ if [ "$rposition" -eq 0 ]; then
     transform_ring_position_in_index 1
 
     if [ "$rindex" -eq -1 ]; then
-        echo "actually, the buffer is empty"
-        exit 0
+        exit 4
     fi 
 
     if [ "$remove" = false ]; then
@@ -180,7 +209,8 @@ if [ "$rposition" -eq 0 ]; then
                 }
             }
         ' "${files[$((1-findex))]}" "${files[$findex]}")
-        
+    	
+    	logmsg="${logmsg}\nAll strings as captured!"
         dispatch_output "$out"
         exit 0
     else
@@ -188,11 +218,10 @@ if [ "$rposition" -eq 0 ]; then
         awk -v RS='\0' -v ORS='\0' '{ print "String removed!" }' "${files[$findex]}" > "$tmpfile" && mv "$tmpfile" "${files[$findex]}"
         awk -v RS='\0' -v ORS='\0' '{ print "String removed!" }' "${files[$((1-findex))]}" > "$tmpfile" && mv "$tmpfile" "${files[$((1-findex))]}"
         
-        echo -e "\x1B[1;31;49mAll strings marked as removed!\x1B[0m"
+        logmsg="${logmsg}\nAll strings as removed!"
         exit 0
     fi
 fi
-
 
 
 
@@ -200,8 +229,7 @@ fi
 transform_ring_position_in_index "$rposition"
 
 if [ "$rindex" -lt 0 ]; then
-    echo "actually, the position $rposition isn't written"
-    exit 0
+    exit 5
 fi
 
 # Estraiamo la stringa target (serve sia per la stampa semplice, sia se facciamo remove+load)
@@ -217,12 +245,10 @@ target_string=$(awk -v RS='\0' -v idx="$rindex" -v is_load="$load" -v n="$rposit
     }
 ' "${files[$findex]}")
 
-if [ "$remove" = false ]; then
-    # Solo lettura/load
-    dispatch_output "$target_string"
-    exit 0
-else
-    # Rimozione della stringa specifica
+
+if [ "$remove" = true  ]
+then
+	# Rimozione della stringa specifica
     awk -v RS='\0' -v ORS='\0' -v idx="$rindex" '
         NR == idx {
             print "String removed!"
@@ -230,11 +256,31 @@ else
         }
         { print }
     ' "${files[$findex]}" > "$tmpfile" && mv "$tmpfile" "${files[$findex]}"
-    
-    # Se l'utente ha chiesto SIA -r SIA -l, carichiamo comunque la stringa rimossa in xclip
-    dispatch_output "$target_string"
-    exit 0
+
+    logmsg="${logmsg}\nSpecified string removed"
 fi
- 
+
+dispatch_output "$target_string"
+exit 0
+
+
+# if [ "$remove" = false ]; then
+    # Solo lettura/load
+    # dispatch_output "$target_string"
+    # exit 0
+# else
+    # Rimozione della stringa specifica
+    # awk -v RS='\0' -v ORS='\0' -v idx="$rindex" '
+        # NR == idx {
+            # print "String removed!"
+            # next
+        # }
+        # { print }
+    # ' "${files[$findex]}" > "$tmpfile" && mv "$tmpfile" "${files[$findex]}"
+    # 
+    # Se l'utente ha chiesto SIA -r SIA -l, carichiamo comunque la stringa rimossa in xclip
+    # dispatch_output "$target_string"
+    # exit 0
+# fi 
  
  
